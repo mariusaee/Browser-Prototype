164495d090072d52d8c393dc76eed0e979576b38
Marius <mariusae@me.com>
2023-06-11 13:59:51 +0400

    CharacterSet

---
+++ Browser Prototype/Extensions/CharacterSet+Extension.swift
@@ -0,0 +1,12 @@
+//
+//  CharacterSet+Extension.swift
+//  Browser Prototype
+//
+//  Created by Marius Malyshev on 11.06.2023.
+//
+
+import Foundation
+
+extension CharacterSet {
+    public static let URLAllowed = CharacterSet(charactersIn: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-._~:/?#[]@!$&'()*+,;=%")
+}
